4cb07163e2900889bd0ee508e1e1d4620e90b4af
Hunghh90 <hungbkvn9x1@gmail.com>
2023-01-05 18:45:42 +0700

    up

---
+++ demo.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Hello world";
+$x = 10;
+$x = "Hello";
+$arr[0] = 15;
+$arr[1] = 20;
+$arr[2] = 30;
+for($i = 0;$i<count($arr);$i++)
+{
+    //$arr[$i];
+}
+foreach ($arr as $a)
+{
+    // $a <=> $arr[$i];
+}
